fd596b13a678f40f60fe41970d9528c6925c0c17
RodrigoCruz1154 <00115417@uca.edu.sv>
2018-10-12 08:53:51 -0600

    dos

---
+++ asset/js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load',()=>{
+    /* Ejercicio 1 */
+    var container = document.getElementById("c1");
+    container.innerHTML = "¡Hola Mundo!";
+    container.style.backgroundColor = "#000000";
+    container.style.color = "#1cb723";
+    container.style.width = "200px";
+    container.style.height = "200px";
+
+    container.innerHTML = "<input type=”text” placeholder=’escriba su texto’>";
+
+    /*Ejercicio 2*/
+    
+})
